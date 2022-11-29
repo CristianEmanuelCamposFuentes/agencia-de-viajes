7a3eec7e010868b90e1497c41041eaff52d460c8
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2022-11-28 23:25:25 -0300

    add index.js

---
+++ agenciaViajesNode/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import router from './routes/index.js';
+
+
+const app = express();
+
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+// Agregar router
+app.use('/', router);
+
+
+// app.get('/', (request, response) => {
+//     response.send('Hola Mundo');
+//     response.json({
+//         id: 1
+//     });
+//     response.render()
+// })
+
+
+app.listen(() =>{
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+})
