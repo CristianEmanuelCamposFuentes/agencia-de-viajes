1b6ba2e81d86cec28888dec6688effed9f4e271e
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2022-12-16 16:50:52 -0300

    Create Main Layout

--- agenciaViajesNode/routes/index.js
+++ agenciaViajesNode/routes/index.js
@@ -3,15 +3,17 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { // Request - lo que enviamos : Response - Lo que express nos responde
-    res.send('Inicio');
+    res.render('inicio');
 });
 
 router.get('/nosotros', (req, res) => { // Request - lo que enviamos : Response - Lo que express nos responde
-    res.send('Nosotros');
+    const viajes = 'Cambiando de texto';
+
+    res.render('nosotros', {
+        viajes
+    });
 });
 
-router.get('/contacto', (req, res) => { // Request - lo que enviamos : Response - Lo que express nos responde
-    res.send('Contacto');
-});
+
 
 export default router;
