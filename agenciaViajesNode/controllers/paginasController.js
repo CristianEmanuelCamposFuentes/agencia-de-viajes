276e72d2bd9a57b5945652f1a5f2835692783bb1
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2022-12-30 08:35:21 -0300

    Create Viajes block

--- agenciaViajesNode/controllers/paginasController.js
+++ agenciaViajesNode/controllers/paginasController.js
@@ -2,11 +2,24 @@ import { Viaje } from "../models/Viaje.js";
 import { Testimonial } from "../models/Testimoniales.js";
 
 
-const paginaInicio = (req, res) => { 
-    res.render('inicio', {
-        pagina: 'Inicio',
-        clase: 'home'
-    });
+const paginaInicio = async (req, res) => { 
+
+    // Consultar 3 viajes del modelo viaje
+
+    try {
+        const viajes = await Viaje.findAll({limit:3});
+
+        res.render('inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    
 }
 
 const paginaNosotros = (req, res) => {
